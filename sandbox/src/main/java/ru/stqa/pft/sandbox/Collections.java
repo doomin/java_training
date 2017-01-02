dd4ae9498c1949ee952e1d71efd0a5bbd585094b
dominik.pszczola@gmail.com <dominik.pszczola@gmail.com>
2017-01-02 19:12:00 +0100

    Assert on group count added
    Assertion for group count added for creation, modification and group
    deletion.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,20 +12,14 @@ public class Collections {
   public static void main (String[] args) {
     String[] langs = {"Java", "C#","Python","PHP"};
 
-    List<String> languages = new ArrayList<String>();
-    languages.add("Java");
-    languages.add("C#");
-    languages.add("Python");
-    languages.add("PHP");
-
-
+    List<String> languages = Arrays.asList("Java", "C#","Python","PHP");
 
     for (int i = 0; i < langs.length; i++){
       System.out.println("Chce nauczyc sie " + langs[i]);
     }
 
     for (String l: languages) {
-      System.out.println("Chce nauczyc sie " + l);
+      System.out.println("Tu z uzyciem listy - Chce nauczyc sie " + l);
     }
   }
 }
