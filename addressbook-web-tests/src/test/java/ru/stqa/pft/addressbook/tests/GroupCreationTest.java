5b3837aeffbbed5dffebcc867ea21ef023f31caf
dominik.pszczola@gmail.com <dominik.pszczola@gmail.com>
2017-01-02 19:21:43 +0100

    Parameter added to the selectGroup method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -9,10 +9,10 @@ public class GroupCreationTest extends TestBase{
   @Test
   public void testGroupCreation() {
 
-    
+
     app.getNavigationHelper().goToGroupPage();
     int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().createGroup(new GroupData("test2", null, null));
+    app.getGroupHelper().createGroup(new GroupData("test4", null, null));
     int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after, before + 1);
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -16,9 +16,9 @@ public class GroupModificationTest extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test12", "test13"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test21", "test12", "test13"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
