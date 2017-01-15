e5553682b76adcfd87bd49e42d4df250af5bebb0
doomin <dominik.pszczola@gmail.com>
2017-01-15 16:25:40 +0100

    ContactDataGenerator has been created based on the GroupDataGenerator
    The available formats are csv, xml and json.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenertor.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenertor.java
@@ -1,7 +1,133 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Dominik on 2017-01-15.
  */
 public class ContactDataGenertor {
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenertor generator = new ContactDataGenertor();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        if(format.equals("csv")) {
+            saveAsCsv(contacts, new File(file));
+        } else if (format.equals("xml")){
+            saveAsXml(contacts,new File(file));
+        } else if (format.equals("json")){
+            saveAsJson(contacts,new File(file));
+        }else {
+            System.out.println("Unrecognized format" + format);
+        }
+    }
+
+    private void saveAsJson(List<ContactData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+    }
+
+    private void saveAsXml(List<ContactData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getMiddlename(), contact.getLastname(),
+            contact.getNickname(), contact.getTitle(), contact.getCompany(), contact.getAddress(), contact.getHome(),
+                    contact.getMobile(), contact.getWork(), contact.getEmail(), contact.getEmail2(), contact.getEmail3(),
+                    contact.getAddress2(), contact.getGroup()));
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++){
+            contacts.add(new ContactData()
+                    .withFirstName(generateRandomString())
+                    .withMiddlename(generateRandomString())
+                    .withLastName(generateRandomString())
+                    .withNickName(generateRandomString())
+                    .withTitle(generateRandomString())
+                    .withCompany(generateRandomString())
+                    .withAddress(generateRandomString() + " " + generateRandomNumber() + " street\n" + generateRandomNumber() + "-" + generateRandomNumber() + "\n" + generateRandomString())
+                    .withHome(generateRandomNumber() + "-" + generateRandomNumber() + "-" + generateRandomNumber())
+                    .withMobile(generateRandomNumber() + "-" + generateRandomNumber() + "-" + generateRandomNumber())
+                    .withWork(generateRandomNumber() + "-" + generateRandomNumber() + "-" + generateRandomNumber())
+                    .withEmail(generateRandomString() + "@" + generateRandomString())
+                    .withEmail2(generateRandomString() + "@" + generateRandomString())
+                    .withEmail3(generateRandomString() + "@" + generateRandomString())
+                    .withAddress2(generateRandomString() + " " + generateRandomNumber() + " street\n" + generateRandomNumber() + "-" + generateRandomNumber() + "\n" + generateRandomString())
+                    .withGroup("test 1")
+            );
+
+        }
+        return contacts;
+    }
+
+    protected String generateRandomString(){
+        String saltchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 5) {
+            int index = (int) (rnd.nextFloat() * saltchars.length());
+            salt.append(saltchars.charAt(index));
+        }
+        String rndStr = salt.toString();
+        return rndStr;
+    }
+
+    protected String generateRandomNumber(){
+        String saltchars = "1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 3) {
+            int index = (int) (rnd.nextFloat() * saltchars.length());
+            salt.append(saltchars.charAt(index));
+        }
+        String rndStr = salt.toString();
+        return rndStr;
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,28 +1,47 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.io.File;
 
 @XStreamAlias("contact")
 public class ContactData {
+    @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+    @Expose
   private String group;
+    @Expose
   private String firstname;
+    @Expose
   private String middlename;
+    @Expose
   private String lastname;
+    @Expose
   private String nickname;
+    @Expose
   private String title;
+    @Expose
   private String company;
+    @Expose
   private String address;
+    @Expose
   private String address2;
+    @Expose
   private String allAddress;
+    @Expose
   private String home;
+    @Expose
   private String mobile;
+    @Expose
   private String work;
   private String allPhones;
+    @Expose
   private String email;
+    @Expose
   private String email2;
+    @Expose
   private String email3;
   private String allEmails;
   private String allDetails;
