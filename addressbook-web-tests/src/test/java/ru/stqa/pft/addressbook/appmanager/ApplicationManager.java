2814cba3702a61e96651e431a520d621c44f12d3
doomin <dominik.pszczola@gmail.com>
2017-01-22 19:26:18 +0100

    GroupModificationTest updated to comapre data from database

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -26,6 +26,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -36,6 +37,9 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
      properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+     dbHelper = new DbHelper();
+
     if (Objects.equals(browser, BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
     } else if (Objects.equals(browser, BrowserType.CHROME)){
@@ -68,5 +72,9 @@ public class ApplicationManager {
 
   public NavigationHelper goTo() {
     return navigationHelper;
+  }
+
+  public DbHelper db(){
+    return dbHelper;
   }
 }
