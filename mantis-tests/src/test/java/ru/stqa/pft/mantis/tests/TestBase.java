b2264bdc85fa6e3aa2778cbf848bc06b2d09304f
doomin <dominik.pszczola@gmail.com>
2017-01-29 21:30:13 +0100

    FTP hepler created and test for updating conf.php file added

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 /**
  * Created by doomin on 12.12.16.
@@ -18,11 +21,13 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "mantisbt-2.0.0/config/config_inc.php", "mantisbt-2.0.0/config/config_inc.php.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
-    app.stop();
+  public void tearDown() throws IOException {
+      app.ftp().restore("mantisbt-2.0.0/config/config_inc.php.bak","mantisbt-2.0.0/config/config_inc.php");
+      app.stop();
   }
 }
 
