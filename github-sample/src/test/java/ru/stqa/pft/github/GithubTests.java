81b5d778bd8f5ec155f3d45b3e4f568d54df4c84
doomin <dominik.pszczola@gmail.com>
2017-02-05 00:05:18 +0100

    Github tests added for viewing the repos

--- github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -14,7 +14,9 @@ public class GithubTests {
     @Test
     public void testCommits() throws IOException {
         Github github = new RtGithub("e722b2cdd8e28342a1fea58ee94b7ca6500388a8");
-        RepoCommits commits = github.repos().get(new Coordinates.Simple("doomin", "java_training")).commits();
+        RepoCommits commits = github.repos()
+                .get(new Coordinates.Simple("doomin", "java_training"))
+                .commits();
 
         for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
                     System.out.println(new RepoCommit.Smart(commit).message());
